8bce03d13fb2d6215b50619898e824ca94f45ece
Yen Tran <tran.yentran@gmail.com>
2017-08-24 19:19:08 -0700

    front-end React for swag shop

---
+++ src/services/notification-service.js
@@ -0,0 +1,49 @@
+export const NOTIF_WISHLIST_CHANGED = "notif_wishlist_changed";
+
+var observers = {};
+let instance = null;
+
+class NotificationService {
+	constructor() {
+		if (!instance) {
+			instance = this;
+		}
+		return instance;
+	}
+	
+	removeObserver = (observer, notifName) => {
+		var obs = observers[notifName];
+		
+		if (obs) {
+			for (var i = 0; i < obs.length; i++) {
+				if (observer === obs[i].observer) {
+					obs.splice(i, 1);
+					observers[notifName] = obs;
+					break;
+				}
+			}
+		}
+	}
+	
+	addObserver = (notifName, observer, callback) => {
+		let obs = observers[notifName];
+		
+		if (!obs) {
+			observers[notifName] = [];
+		}
+		
+		let obj = {observer: observer, callback: callback}; 
+		observers[notifName].push(obj);
+	}
+	
+	postNotification = (notifName, data) => {
+		let obs = observers[notifName];
+		
+		for (var i = 0; i < obs.length; i++) {
+			var obj = obs[i];
+			obj.callback(data);
+		}
+	}
+}
+
+export default NotificationService;
